6952e1aa6367f92c040f6dc87812e7a2bda2d782
Connor Shea <connor.james.shea@gmail.com>
2018-04-12 17:25:40 -0600

    Remove unnecessary code from the seeds.rb file, slight improvements to the JSON parsing script.

--- lib/parse_json.rb
+++ lib/parse_json.rb
@@ -1,10 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# This script is for screwing around with parsing the MDN Browser Compat Data JSON file.
+# It's very bad right now.
+# I'm so sorry.
 
 require 'json'
 require 'pp'
@@ -28,58 +24,15 @@ def get_browser_keys(browsers)
   end
 end
 
-# puts browser_keys
-
-# css.each do |key, feature|
-#   5.times { puts " " }
-#   puts key.to_s
-#   feature.each do |subkey, subfeature|
-#     puts subkey
-#     subfeature[:__compat][:support].each do |browser_name, browser|
-#       puts browser_name
-#       # If the browser object has more than one subvalue it either has multiple
-#       # version notes or a separate note about flags/notes, we only want info
-#       # about what version it was added in.
-#       if browser.length > 1
-#         browser.each do |k, v|
-#           if k == :version_added
-#             puts v
-#           end
-#         end
-#       else
-#         puts browser[:version_added]
-#       end
-#     end
-#     5.times { puts " " }
-#   end
-# end
-
-# css.each do |k, v|
-#   # puts "k: #{k}"
-#   # puts "v: #{v}"
-
-#   v.each do |k2, v2|
-#     puts "k2: #{k2}"
-#     puts "v2: #{v2}"
-
-#     v2.each do |k3, v3|
-#       puts "k3: #{k3}"
-#       puts "v3: #{v3}"
-#     end
-#   end
-# end
-
 def parse_browser_data_schema(data_object)
-  # puts object
+  get_browser_keys(data_object["browsers"])
 
+  # Full version:
   # top_level_schema = {
   #   css: ["at-rules", "properties", "selectors", "types"],
   #   html: ["elements", "global_attributes"],
   #   javascript: ["builtins", "classes", "functions", "grammar", "operators", "statements"]
   # }
-
-  get_browser_keys(data_object["browsers"])
-
   top_level_schema = {
     css: ["at-rules", "properties", "selectors", "types"]
   }
@@ -108,8 +61,8 @@ def parse_browser_data_schema(data_object)
             puts "Support for #{key3} in #{browser_key}"
             # This can either be a Hash or an Array, depending on the specific case.
             # puts data_object[key.to_s][key2.to_s][key3.to_s]["__compat"]["support"][browser_key.to_s]
-
-            # If it's an array.
+            # 
+            # If it's an Array.
             if data_object[key.to_s][key2.to_s][key3.to_s]["__compat"]["support"][browser_key.to_s].kind_of?(Array)
               # puts "IT'S AN ARRAY!"
               data_object[key.to_s][key2.to_s][key3.to_s]["__compat"]["support"][browser_key.to_s].each do |info|
@@ -132,9 +85,6 @@ def parse_browser_data_schema(data_object)
           end
         end
       end
-
-      # data_object_dug = data_object.dig(key.to_s, key2.to_s)
-      # puts data_object_dug
     end
   end
 end
