428f4e0af0d3efc411b97cd305463a886d7ff3ed
Connor Shea <connor.james.shea@gmail.com>
2018-04-16 12:58:06 -0600

    Improve the code for resorting the releases.

--- app/controllers/browsers_controller.rb
+++ app/controllers/browsers_controller.rb
@@ -6,24 +6,23 @@ class BrowsersController < ApplicationController
       # https://stackoverflow.com/questions/808318/sorting-a-ruby-array-of-objects-by-an-attribute-that-could-be-nil
       # Sorts the array and leaves nil values at the end.
       browser.releases = browser.releases.sort { |a, b| a["release_date"] && b["release_date"] ? a["release_date"] <=> b["release_date"] : a["release_date"] ? -1 : 1 }
-      
-      # Collect all release dates which are nil so we can sort them separately.
-      nil_release_dates = []
+
+      nil_values = []
+      browser_releases_with_release_dates = []
       browser.releases.each_with_index do |release_info, i|
+        # Collect all release dates which are nil so we can sort them
+        # separately.
         if release_info["release_date"].nil?
-          nil_release_dates.push(i)
+          nil_values << browser.releases[i]
+        # Collect all releases with release date values.
+        else
+          browser_releases_with_release_dates << browser.releases[i]
         end
       end
 
-      nil_values = []
-      nil_release_dates.each do |i|
-        nil_values << browser.releases[i]
-      end
-
-      # Remove all releases with nil release dates from the browser releases.
-      nil_values.each do |key, value|
-        browser.releases.delete(key)
-      end
+      # Replace browser.releases with the array of releases that have valid
+      # release dates. This is a workaround means of deleting all nil values.
+      browser.releases = browser_releases_with_release_dates
 
       # Sort the nil_values array by the version number, then append it to the
       # browser releases.
