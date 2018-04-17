a94d49c251af1c5f57ffc4d7b16882780d231648
Connor Shea <connor.james.shea@gmail.com>
2018-04-17 11:06:12 -0600

    Improve features table design, fix values not being output correctly.

--- app/helpers/features_helper.rb
+++ app/helpers/features_helper.rb
@@ -2,18 +2,18 @@ module FeaturesHelper
   def version_added_parser(browser_info)
     if browser_info.kind_of?(Array)
       version_added = browser_info[0]['version_added']
-    elsif browser_info.kind_of?(Array)
+    elsif browser_info.kind_of?(Hash)
       version_added = browser_info['version_added']
     end
 
     # it would appear false values are changed to nil by Ruby at some point
     case version_added
-      when 'true'
+      when true
         return "Yes"
-      when 'false'
+      when false
         return "No"
       when nil
-        return "Unknown"
+        return "?"
       else
         return version_added
       end
