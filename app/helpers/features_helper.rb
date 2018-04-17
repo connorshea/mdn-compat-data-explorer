0ffcfd8fada5231212ebade929df99798bcd7ee3
Connor Shea <connor.james.shea@gmail.com>
2018-04-16 21:21:35 -0600

    Improve the features table more.

--- app/helpers/features_helper.rb
+++ app/helpers/features_helper.rb
@@ -1,9 +1,21 @@
 module FeaturesHelper
   def version_added_parser(browser_info)
     if browser_info.kind_of?(Array)
-      return browser_info[0]['version_added']
+      version_added = browser_info[0]['version_added']
     elsif browser_info.kind_of?(Array)
-      return browser_info['version_added']
+      version_added = browser_info['version_added']
     end
+
+    # it would appear false values are changed to nil by Ruby at some point
+    case version_added
+      when 'true'
+        return "Yes"
+      when 'false'
+        return "No"
+      when nil
+        return "Unknown"
+      else
+        return version_added
+      end
   end
 end
