a251c3d28bea6d5aa3273b21d2cc1b0f0c15dcbd
Connor Shea <connor.james.shea@gmail.com>
2018-04-17 11:14:38 -0600

    Add a comment for future use.

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -5,6 +5,9 @@ class FeaturesController < ApplicationController
     # ActiveRecord Query for all css items:
     # Feature.where("name ~* ?", '^css.*')
 
+    # Query for all features with an MDN URL:
+    # @features_with_mdn = Feature.where.not(mdn_url: nil)
+
     @feature_count = Feature.all.count
   end
 end
