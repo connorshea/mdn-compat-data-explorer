de59b108e480c9fc67a0d8c475934b4a4237f19b
Connor Shea <connor.james.shea@gmail.com>
2018-04-18 14:11:16 -0600

    Fix a test case.

--- test/controllers/features_controller_test.rb
+++ test/controllers/features_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class FeaturesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get features_index_url
+    get features_url
     assert_response :success
   end
 
