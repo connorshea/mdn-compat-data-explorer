2360bfcae3ba98659be263762c4a40e341d948a6
Connor Shea <connor.james.shea@gmail.com>
2018-04-19 11:45:00 -0600

    Add subpages for each category of feature.

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -1,7 +1,20 @@
 class FeaturesController < ApplicationController
-  def index
-    @features = Feature.where("name ~* ?", '^css.*').page params[:page]
+  BROWSERS = [
+    "chrome",
+    "chrome_android",
+    "edge",
+    "edge_mobile",
+    "firefox",
+    "firefox_android",
+    "ie",
+    "nodejs",
+    "opera",
+    "safari",
+    "safari_ios",
+    "webview_android"
+  ]
 
+  def index
     # ActiveRecord Query for all css items:
     # Feature.where("name ~* ?", '^css.*')
 
@@ -11,21 +24,56 @@ class FeaturesController < ApplicationController
     # Query for all features with a description:
     # @features_with_description_count = Feature.where.not(description: nil).count
 
+    @features = Feature.all.page params[:page]
     @feature_count = Feature.all.count
+    @browsers = BROWSERS
+  end
 
-    @browsers = [
-      "chrome",
-      "chrome_android",
-      "edge",
-      "edge_mobile",
-      "firefox",
-      "firefox_android",
-      "ie",
-      "nodejs",
-      "opera",
-      "safari",
-      "safari_ios",
-      "webview_android"
-    ]
+  def api
+    @features = Feature.where("name ~* ?", '^api.*').page params[:page]
+    @feature_count = Feature.where("name ~* ?", '^api.*').count
+    @browsers = BROWSERS
+  end
+
+  def css
+    @features = Feature.where("name ~* ?", '^css.*').page params[:page]
+    @feature_count = Feature.where("name ~* ?", '^css.*').count
+    @browsers = BROWSERS
+  end
+
+  def html
+    @features = Feature.where("name ~* ?", '^html.*').page params[:page]
+    @feature_count = Feature.where("name ~* ?", '^html.*').count
+    @browsers = BROWSERS
+  end
+
+  def http
+    @features = Feature.where("name ~* ?", '^http.*').page params[:page]
+    @feature_count = Feature.where("name ~* ?", '^http.*').count
+    @browsers = BROWSERS
+  end
+
+  def javascript
+    @features = Feature.where("name ~* ?", '^javascript.*').page params[:page]
+    @feature_count = Feature.where("name ~* ?", '^javascript.*').count
+    @browsers = BROWSERS
+  end
+
+  def svg
+    @features = Feature.where("name ~* ?", '^svg.*').page params[:page]
+    @feature_count = Feature.where("name ~* ?", '^svg.*').count
+    @browsers = BROWSERS
+  end
+
+  def webdriver
+    @features = Feature.where("name ~* ?", '^webdriver.*').page params[:page]
+    @feature_count = Feature.where("name ~* ?", '^webdriver.*').count
+    @browsers = BROWSERS
+  end
+
+  def webextensions
+    @features = Feature.where("name ~* ?", '^webextensions.*').page params[:page]
+    @feature_count = Feature.where("name ~* ?", '^webextensions.*').count
+    @browsers = BROWSERS
   end
 end
