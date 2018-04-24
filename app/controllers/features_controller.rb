a8bdfbe93403c48c79832aa656fee00e23346387
Connor Shea <connor.james.shea@gmail.com>
2018-04-24 10:47:52 -0600

    Update features to use scopes in the Features model.

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -29,15 +29,6 @@ class FeaturesController < ApplicationController
   ]
 
   def index
-    # ActiveRecord Query for all css items:
-    # Feature.where("name ~* ?", '^css.*')
-
-    # Query for all features with an MDN URL:
-    # @features_with_mdn_count = Feature.where.not(mdn_url: nil).count
-
-    # Query for all features with a description:
-    # @features_with_description_count = Feature.where.not(description: nil).count
-
     @features = Feature.all.page params[:page]
     @feature_count = Feature.all.count
     @browsers = BROWSERS_PLUS_NODE
@@ -46,56 +37,56 @@ class FeaturesController < ApplicationController
   end
 
   def api
-    @features = Feature.where("name ~* ?", '^api.*').page params[:page]
-    @feature_count = Feature.where("name ~* ?", '^api.*').count
+    @features = Feature.api.page params[:page]
+    @feature_count = Feature.api.count
     @browsers = BROWSERS
   end
 
   def css
-    @features = Feature.where("name ~* ?", '^css.*').page params[:page]
-    @feature_count = Feature.where("name ~* ?", '^css.*').count
+    @features = Feature.css.page params[:page]
+    @feature_count = Feature.css.count
     @browsers = BROWSERS
   end
 
   def html
-    @features = Feature.where("name ~* ?", '^html.*').page params[:page]
-    @feature_count = Feature.where("name ~* ?", '^html.*').count
+    @features = Feature.html.page params[:page]
+    @feature_count = Feature.html.count
     @browsers = BROWSERS
   end
 
   def http
-    @features = Feature.where("name ~* ?", '^http.*').page params[:page]
-    @feature_count = Feature.where("name ~* ?", '^http.*').count
+    @features = Feature.http.page params[:page]
+    @feature_count = Feature.http.count
     @browsers = BROWSERS
   end
 
   def javascript
-    @features = Feature.where("name ~* ?", '^javascript.*').page params[:page]
-    @feature_count = Feature.where("name ~* ?", '^javascript.*').count
+    @features = Feature.javascript.page params[:page]
+    @feature_count = Feature.javascript.count
     @browsers = BROWSERS_PLUS_NODE
   end
 
   def mathml
-    @features = Feature.where("name ~* ?", '^mathml.*').page params[:page]
-    @feature_count = Feature.where("name ~* ?", '^mathml.*').count
+    @features = Feature.mathml.page params[:page]
+    @feature_count = Feature.mathml.count
     @browsers = BROWSERS
   end
 
   def svg
-    @features = Feature.where("name ~* ?", '^svg.*').page params[:page]
-    @feature_count = Feature.where("name ~* ?", '^svg.*').count
+    @features = Feature.svg.page params[:page]
+    @feature_count = Feature.svg.count
     @browsers = BROWSERS
   end
 
   def webdriver
-    @features = Feature.where("name ~* ?", '^webdriver.*').page params[:page]
-    @feature_count = Feature.where("name ~* ?", '^webdriver.*').count
+    @features = Feature.webdriver.page params[:page]
+    @feature_count = Feature.webdriver.count
     @browsers = BROWSERS
   end
 
   def webextensions
-    @features = Feature.where("name ~* ?", '^webextensions.*').page params[:page]
-    @feature_count = Feature.where("name ~* ?", '^webextensions.*').count
+    @features = Feature.webextensions.page params[:page]
+    @feature_count = Feature.webextensions.count
     @browsers = BROWSERS
   end
 
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,10 @@
 class WelcomeController < ApplicationController
   def index
-    # ActiveRecord Query for all css items:
-    # Feature.where("name ~* ?", '^css.*')
-
     # Query for all features with an MDN URL:
-    @features_with_mdn_count = Feature.where.not(mdn_url: nil).count
+    @features_with_mdn_count = Feature.has_mdn_url.count
 
     # Query for all features with a description:
-    @features_with_description_count = Feature.where.not(description: nil).count
+    @features_with_description_count = Feature.has_description.count
 
     #  Features where chrome has no set version number.
     # @features_with_chrome_count = Feature.where(chrome: {"version_added"=>nil}).count
@@ -21,9 +18,9 @@ class WelcomeController < ApplicationController
 
     @features_with_webview_android_count = Feature.where.not(webview_android: nil).count
 
-    @standard_features = Feature.where(standard_track: true).count
-    @experimental_features = Feature.where(experimental: true).count
-    @deprecated_features = Feature.where(deprecated: true).count
+    @standard_features = Feature.is_on_standard_track.count
+    @experimental_features = Feature.is_experimental.count
+    @deprecated_features = Feature.is_deprecated.count
 
     @feature_count = Feature.all.count
   end
