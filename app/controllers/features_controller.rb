960c11736e887895f7e35cef5ff8438fff6c029d
Connor Shea <connor.james.shea@gmail.com>
2018-04-24 11:31:31 -0600

    Add category filtering to the search page, doesn't quite work yet.

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -92,10 +92,30 @@ class FeaturesController < ApplicationController
 
   def search
     if params[:query].present?
-      @features = Feature.search(params[:query]).page(params[:page])
+      features = Feature.search(params[:query])
     else
-      @features = Feature.all.page(params[:page])
+      features = Feature.all
     end
+
+    @categories = {
+      api: "API",
+      css: "CSS",
+      html: "HTML",
+      http: "HTTP",
+      javascript: "JavaScript",
+      mathml: "MathML",
+      svg: "SVG",
+      webdriver: "WebDriver",
+      webextensions: "WebExtensions"
+    }
+
+    @categories.keys.each do |category|
+      if params[category].present? && params[category]
+        features = features.public_send(category)
+      end
+    end
+
+    @features = features.page(params[:page])
 
     @browsers = BROWSERS_PLUS_NODE
 
