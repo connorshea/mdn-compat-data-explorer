88fa86a2e0ccec93beb816ffc5c13400c69cdd75
Connor Shea <connor.james.shea@gmail.com>
2018-04-28 17:26:24 -0600

    Use factories instead of traits, add test for firefox_false.

--- test/factories/features.rb
+++ test/factories/features.rb
@@ -33,47 +33,47 @@ FactoryBot.define do
   factory :feature, class: Feature do
     sequence(:name) { |n| "feature#{n}" }
 
-    trait :has_description do
+    factory :has_description do
       description "Lorem ipsum"
     end
 
-    trait :has_mdn_url do
+    factory :has_mdn_url do
       mdn_url "https://developer.mozilla.org/docs/Web/HTML/Element/header"
     end
 
-    trait :deprecated_true do
+    factory :deprecated_true do
       deprecated true
     end
 
-    trait :deprecated_false do
+    factory :deprecated_false do
       deprecated false
     end
 
-    trait :experimental_true do
+    factory :experimental_true do
       experimental true
     end
 
-    trait :experimental_false do
+    factory :experimental_false do
       experimental false
     end
 
-    trait :standard_track_true do
+    factory :standard_track_true do
       standard_track true
     end
 
-    trait :standard_track_false do
+    factory :standard_track_false do
       standard_track false
     end
 
-    trait :firefox_true_simple_one do
+    factory :firefox_true_simple_one do
       firefox { { version_added: true } }
     end
 
-    trait :firefox_true_simple_two do
+    factory :firefox_true_simple_two do
       firefox { { version_added: "1" } }
     end
 
-    trait :firefox_true_complex_one do
+    factory :firefox_true_complex_one do
       firefox {
         {
           version_added: true,
@@ -82,7 +82,7 @@ FactoryBot.define do
       }
     end
 
-    trait :firefox_true_complex_two do
+    factory :firefox_true_complex_two do
       firefox {
         [
           {
@@ -96,21 +96,8 @@ FactoryBot.define do
       }
     end
 
-    factory :feature_with_description, traits: [:has_description]
-    factory :feature_with_mdn_url, traits: [:has_mdn_url]
-    
-    factory :feature_deprecated, traits: [:deprecated_true]
-    factory :feature_not_deprecated, traits: [:deprecated_false]
-
-    factory :feature_standard_track, traits: [:standard_track_true]
-    factory :feature_not_standard_track, traits: [:standard_track_false]
-
-    factory :feature_experimental, traits: [:experimental_true]
-    factory :feature_not_experimental, traits: [:experimental_false]
-
-    factory :feature_firefox_true_simple_one, traits: [:firefox_true_simple_one]
-    factory :feature_firefox_true_simple_two, traits: [:firefox_true_simple_two]
-    factory :feature_firefox_true_complex_one, traits: [:firefox_true_complex_one]
-    factory :feature_firefox_true_complex_two, traits: [:firefox_true_complex_two]
+    factory :firefox_false do
+      firefox { { version_added: false } }
+    end
   end
 end
--- test/models/feature_test.rb
+++ test/models/feature_test.rb
@@ -33,7 +33,7 @@ require 'test_helper'
 
 class FeatureTest < ActiveSupport::TestCase
   test "has_description scope does not return features without descriptions" do
-    feature_with_description = create(:feature_with_description)
+    feature_with_description = create(:has_description)
     feature = create(:feature)
     
     assert_not_includes(Feature.has_description, Feature.find_by!(name: feature.name))
@@ -41,7 +41,7 @@ class FeatureTest < ActiveSupport::TestCase
   end
 
   test "has_mdn_url scope does not return features without an MDN URL" do
-    feature_with_mdn_url = create(:feature_with_mdn_url)
+    feature_with_mdn_url = create(:has_mdn_url)
     feature = create(:feature)
 
     assert_not_includes(Feature.has_mdn_url, Feature.find_by!(name: feature.name))
@@ -49,8 +49,8 @@ class FeatureTest < ActiveSupport::TestCase
   end
 
   test "is_deprecated scope returns only deprecated features" do
-    feature_deprecated = create(:feature_deprecated)
-    feature_not_deprecated = create(:feature_not_deprecated)
+    feature_deprecated = create(:deprecated_true)
+    feature_not_deprecated = create(:deprecated_false)
     feature = create(:feature)
 
     assert_includes(Feature.is_deprecated, Feature.find_by!(name: feature_deprecated.name))
@@ -59,8 +59,8 @@ class FeatureTest < ActiveSupport::TestCase
   end
 
   test "is_not_deprecated scope returns non-deprecated features" do
-    feature_deprecated = create(:feature_deprecated)
-    feature_not_deprecated = create(:feature_not_deprecated)
+    feature_deprecated = create(:deprecated_true)
+    feature_not_deprecated = create(:deprecated_false)
     feature = create(:feature)
 
     assert_includes(Feature.is_not_deprecated, Feature.find_by!(name: feature_not_deprecated.name))
@@ -69,8 +69,8 @@ class FeatureTest < ActiveSupport::TestCase
   end
 
   test "no_deprecation_info scope returns only features without deprecation data" do
-    feature_deprecated = create(:feature_deprecated)
-    feature_not_deprecated = create(:feature_not_deprecated)
+    feature_deprecated = create(:deprecated_true)
+    feature_not_deprecated = create(:deprecated_false)
     feature = create(:feature)
 
     assert_includes(Feature.no_deprecation_info, Feature.find_by!(name: feature.name))
@@ -79,8 +79,8 @@ class FeatureTest < ActiveSupport::TestCase
   end
 
   test "is_on_standard_track scope returns only standard features" do
-    feature_standard_track = create(:feature_standard_track)
-    feature_not_standard_track = create(:feature_not_standard_track)
+    feature_standard_track = create(:standard_track_true)
+    feature_not_standard_track = create(:standard_track_false)
     feature = create(:feature)
 
     assert_includes(Feature.is_on_standard_track, Feature.find_by!(name: feature_standard_track.name))
@@ -89,8 +89,8 @@ class FeatureTest < ActiveSupport::TestCase
   end
 
   test "is_not_on_standard_track scope returns only non-standard features" do
-    feature_standard_track = create(:feature_standard_track)
-    feature_not_standard_track = create(:feature_not_standard_track)
+    feature_standard_track = create(:standard_track_true)
+    feature_not_standard_track = create(:standard_track_false)
     feature = create(:feature)
 
     assert_includes(Feature.is_not_on_standard_track, Feature.find_by!(name: feature_not_standard_track.name))
@@ -99,8 +99,8 @@ class FeatureTest < ActiveSupport::TestCase
   end
 
   test "no_standard_track_info scope returns only features without standard track data" do
-    feature_standard_track = create(:feature_standard_track)
-    feature_not_standard_track = create(:feature_not_standard_track)
+    feature_standard_track = create(:standard_track_true)
+    feature_not_standard_track = create(:standard_track_false)
     feature = create(:feature)
 
     assert_includes(Feature.no_standard_track_info, Feature.find_by!(name: feature.name))
@@ -109,8 +109,8 @@ class FeatureTest < ActiveSupport::TestCase
   end
 
   test "is_experimental scope returns only experimental features" do
-    feature_experimental = create(:feature_experimental)
-    feature_not_experimental = create(:feature_not_experimental)
+    feature_experimental = create(:experimental_true)
+    feature_not_experimental = create(:experimental_false)
     feature = create(:feature)
 
     assert_includes(Feature.is_experimental, Feature.find_by!(name: feature_experimental.name))
@@ -119,8 +119,8 @@ class FeatureTest < ActiveSupport::TestCase
   end
 
   test "is_not_experimental scope returns only non-experimental features" do
-    feature_experimental = create(:feature_experimental)
-    feature_not_experimental = create(:feature_not_experimental)
+    feature_experimental = create(:experimental_true)
+    feature_not_experimental = create(:experimental_false)
     feature = create(:feature)
     
     assert_includes(Feature.is_not_experimental, Feature.find_by!(name: feature_not_experimental.name))
@@ -129,8 +129,8 @@ class FeatureTest < ActiveSupport::TestCase
   end
 
   test "no_experimental_info scope returns only features without experimental data" do
-    feature_experimental = create(:feature_experimental)
-    feature_not_experimental = create(:feature_not_experimental)
+    feature_experimental = create(:experimental_true)
+    feature_not_experimental = create(:experimental_false)
     feature = create(:feature)
 
     assert_includes(Feature.no_experimental_info, Feature.find_by!(name: feature.name))
@@ -138,11 +138,11 @@ class FeatureTest < ActiveSupport::TestCase
     assert_not_includes(Feature.no_experimental_info, Feature.find_by!(name: feature_not_experimental.name))
   end
 
-  test "firefox_true filter returns features that should be true" do
-    feature_firefox_true_simple_one = create(:feature_firefox_true_simple_one)
-    feature_firefox_true_simple_two = create(:feature_firefox_true_simple_two)
-    feature_firefox_true_complex_one = create(:feature_firefox_true_complex_one)
-    feature_firefox_true_complex_two = create(:feature_firefox_true_complex_two)
+  test "firefox_true scope returns features that should be true" do
+    feature_firefox_true_simple_one = create(:firefox_true_simple_one)
+    feature_firefox_true_simple_two = create(:firefox_true_simple_two)
+    feature_firefox_true_complex_one = create(:firefox_true_complex_one)
+    feature_firefox_true_complex_two = create(:firefox_true_complex_two)
     feature = create(:feature)
 
     assert_includes(Feature.firefox_true, Feature.find_by!(name: feature_firefox_true_simple_one.name))
@@ -153,5 +153,13 @@ class FeatureTest < ActiveSupport::TestCase
     # assert_includes(Feature.firefox_true, Feature.find_by!(name: feature_firefox_true_complex_two.name))
 
     assert_not_includes(Feature.firefox_true, Feature.find_by!(name: feature.name))
+  end
+
+  test "firefox_false scope returns features that should be false" do
+    feature_firefox_false = create(:firefox_false)
+    feature = create(:feature)
+
+    assert_includes(Feature.firefox_false, Feature.find_by!(name: feature_firefox_false.name))
+    assert_not_includes(Feature.firefox_false, Feature.find_by!(name: feature.name))
   end
 end
