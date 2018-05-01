be2784ce6281ec702be97e7510a366831d3dd8c9
Connor Shea <connor.james.shea@gmail.com>
2018-05-01 11:13:13 -0600

    Make flags support work using existing kumascript code.

--- app/assets/javascripts/features.js
+++ app/assets/javascripts/features.js
@@ -36,36 +36,88 @@ function parseHashSupportObject(json, item) {
   let contentForPopover = new Map();
 
   if (jsonKeys.includes('partial_implementation')) {
-    contentForPopover.set("Partial implementation", "This is a partial implementation.");
+    contentForPopover.set("partial_implementation", "This is a partial implementation.");
   }
 
   if (jsonKeys.includes('notes')) {
     if (Array.isArray(json.notes)) {
-      contentForPopover.set("Notes", json.notes.join("<br>"));
+      contentForPopover.set("notes", `Notes: ${json.notes.join("<br>")}`);
     } else {
-      contentForPopover.set("Notes", json.notes);
+      contentForPopover.set("notes", `Notes: ${json.notes}`);
     }
   }
   
   if (jsonKeys.includes('alternative_name')) {
-    contentForPopover.set("Alternative Name", "<code>" + json.alternative_name + "</code>");
+    contentForPopover.set("alternative_name", `<code>${json.alternative_name}</code>`);
   }
 
   if (jsonKeys.includes('prefix')) {
-    contentForPopover.set("Requires the vendor prefix", "<code>" + json.prefix + "</code>");
+    contentForPopover.set("prefix", `Requires the vendor prefix: <code>${json.prefix}</code>`);
   }
 
+  // From https://github.com/mdn/kumascript/blob/1b392341c2835e8e73bba8471160c1349f2838dc/macros/Compat.ejs#L188-L263
   if (jsonKeys.includes('flags')) {
-    contentForPopover.set("Flags", json.flags);
+    let output = '';
+
+    if (typeof (json.version_added) === 'string') {
+      output = `From version ${json.version_added}`;
+    }
+    if (typeof (json.version_removed) === 'string') {
+      if (output) {
+        output += ' ';
+        output += `until version ${json.version_removed} (exclusive)`;
+      } else {
+        output = `Until version ${json.version_removed} (exclusive)`;
+      }
+    }
+
+    let start = 'This';
+    if (output) {
+      output += ':';
+      start = ' this';
+    }
+
+    start += ' feature is behind the ';
+    let flagsText = '';
+    let settings = '';
+
+    for (i = 0; i < json.flags.length; i++) {
+      let flag = json.flags[i];
+      let nameString = `<code>${flag.name}</code>`;
+      // value_to_set is optional
+      let valueToSet = '';
+      if (flag.value_to_set) {
+        valueToSet = ` (needs to be set to <code>${flag.value_to_set}</code>)`;
+      }
+      let typeString = '';
+      if (flag.type === 'preference') {
+        typeString = ` preference${valueToSet}`;
+      }
+      if (flag.type === 'compile_flag') {
+        typeString = ` compile flag${valueToSet}`;
+      }
+      if (flag.type === 'runtime_flag') {
+        typeString = ` runtime flag${valueToSet}`;
+      }
+      flagsText += nameString + typeString;
+      if (i != json.flags.length - 1) {
+        flagsText += ' and the ';
+      } else {
+        flagsText += '.';
+      }
+    }
+    output += start + flagsText;
+
+    contentForPopover.set("flags", output);
   }
 
   if (jsonKeys.includes('version_removed')) {
-    contentForPopover.set("Version removed", json.version_removed);
+    contentForPopover.set("version_removed", `Version removed: ${json.version_removed}`);
   }
 
   contentForPopoverHtml = "";
   for (var [key, value] of contentForPopover.entries()) {
-    contentForPopoverHtml += '<p>' + key + ': ' + value + '</p>';
+    contentForPopoverHtml += `<p>${value}</p>`;
   }
 
   item.dataset.content = contentForPopoverHtml;
