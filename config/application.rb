5a5d5c49c1acb6ce601b0b5225722d5234d62a45
Connor Shea <connor.james.shea@gmail.com>
2018-05-03 10:38:32 -0600

    Update MDN Browser Compat Data package to 0.0.34.

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module MdnCompatDataExplorer
     config.load_defaults 5.2
 
     # Set the version of the MDN BCD Package
-    config.mdn_bcd_version = '0.0.33'
+    config.mdn_bcd_version = '0.0.34'
 
     Rails.application.config.assets.configure do |env|
       AutoprefixerRails.uninstall env
