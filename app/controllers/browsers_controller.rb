7ae49979d2af06a2ee6b46ef38cc0c4d4ba9cbf5
Connor Shea <connor.james.shea@gmail.com>
2018-05-07 12:06:42 -0600

    Add a show page for each browser, list all new features for each release. Also add a browser release sorting helper.

--- app/controllers/browsers_controller.rb
+++ app/controllers/browsers_controller.rb
@@ -1,37 +1,14 @@
 class BrowsersController < ApplicationController
   def index
-    @browsers = Browser.all
-
-    @browsers.each do |browser|
-      # https://stackoverflow.com/questions/808318/sorting-a-ruby-array-of-objects-by-an-attribute-that-could-be-nil
-      # Sorts the array and leaves nil values at the end.
-      browser.releases = browser.releases.sort { |a, b| a["release_date"] && b["release_date"] ? a["release_date"] <=> b["release_date"] : a["release_date"] ? -1 : 1 }
-
-      nil_values = []
-      browser_releases_with_release_dates = []
-      browser.releases.each_with_index do |release_info, i|
-        # Collect all release dates which are nil so we can sort them
-        # separately.
-        if release_info["release_date"].nil?
-          nil_values << browser.releases[i]
-        # Collect all releases with release date values.
-        else
-          browser_releases_with_release_dates << browser.releases[i]
-        end
-      end
-
-      # Replace browser.releases with the array of releases that have valid
-      # release dates. This is a workaround means of deleting all nil values.
-      browser.releases = browser_releases_with_release_dates
-
-      # Sort the nil_values array by the version number, then append it to the
-      # browser releases.
-      VersionSorter.sort!(nil_values) { |release| release["version"] }
-      nil_values.each { |release| browser.releases.push(release) }
-    end
+    @browsers = helpers.browser_releases_sorter(Browser.all)
 
     respond_to do |format|
       format.html
     end
+  end
+
+  def show
+    @browser = Browser.find(params[:id])
+    @browser = helpers.browser_releases_sorter(@browser)
   end
 end
