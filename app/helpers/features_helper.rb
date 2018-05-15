c2c0cf9c4987bcb86473ed088ae82cb72da7d9fd
Connor Shea <connor.james.shea@gmail.com>
2018-05-15 16:05:48 -0600

    Add links to feature pages on browser pages.

--- app/helpers/features_helper.rb
+++ app/helpers/features_helper.rb
@@ -73,6 +73,6 @@ module FeaturesHelper
   end
 
   def browser_release_features(browser, version)
-    return Feature.where("#{browser} @> ?", {'version_added': "#{version}"}.to_json).select("name")
+    return Feature.where("#{browser} @> ?", {'version_added': "#{version}"}.to_json).select("name, slug")
   end
 end
