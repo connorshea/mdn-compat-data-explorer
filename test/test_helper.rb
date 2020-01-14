20fdd17c60fa825d8995fed011dd38c8a3ad949e
Connor Shea <connor.james.shea@gmail.com>
2020-01-13 20:08:09 -0700

    Require webdrivers gem in tests.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require 'webdrivers'
 
 class ActiveSupport::TestCase
   include FactoryBot::Syntax::Methods
